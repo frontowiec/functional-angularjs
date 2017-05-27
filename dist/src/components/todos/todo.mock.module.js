dd2f44da02af397305f28b0ef60502d721ecc5c6
Marcin Sirocki <m.sirocki@madkom.pl>
2017-05-27 13:16:32 +0200

    persist state in localstorage

--- dist/src/components/todos/todo.mock.module.js
+++ dist/src/components/todos/todo.mock.module.js
@@ -10,70 +10,78 @@ var todos_constants_1 = require("../../constants/todos.constants");
 var TodoMockModule;
 (function (TodoMockModule) {
     TodoMockModule.name = 'app.todo.mock';
-    TodoMockModule.actions = [
-        {
-            type: todos_constants_1.ADD_TODO,
-            payload: {
-                todo: {
-                    name: 'Task#1',
-                    completed: false,
+    function saveInitDataToLocalStorage() {
+        var actions = [
+            {
+                type: todos_constants_1.ADD_TODO,
+                payload: {
+                    todo: {
+                        name: 'Task#1',
+                        completed: false,
+                        id: 0
+                    }
+                },
+                version: '0.1',
+                createDate: new Date('2017-05-26T12:00:00'),
+                description: 'Add Task#1 to todo list',
+                applied: true
+            },
+            {
+                type: todos_constants_1.ADD_TODO,
+                payload: {
+                    todo: {
+                        name: 'Task#2',
+                        completed: false,
+                        id: 1
+                    }
+                },
+                version: '0.1',
+                createDate: new Date('2017-05-26T12:20:00'),
+                description: 'Add Task#2 to todo list',
+                applied: true
+            },
+            {
+                type: todos_constants_1.TOGGLE_TODO,
+                payload: {
+                    todo: {
+                        name: 'Task#2',
+                        completed: true,
+                        id: 1
+                    }
+                },
+                version: '0.1',
+                createDate: new Date('2017-05-26T12:21:00'),
+                description: 'Change Task#2 status to complete',
+                applied: true
+            },
+            {
+                type: todos_constants_1.REMOVE_TODO,
+                payload: {
                     id: 0
-                }
-            },
-            version: '0.1',
-            createDate: new Date('2017-05-26T12:00:00'),
-            description: 'Add Task#1 to todo list',
-            applied: true
-        },
-        {
-            type: todos_constants_1.ADD_TODO,
-            payload: {
-                todo: {
-                    name: 'Task#2',
-                    completed: false,
-                    id: 1
-                }
-            },
-            version: '0.1',
-            createDate: new Date('2017-05-26T12:20:00'),
-            description: 'Add Task#2 to todo list',
-            applied: true
-        },
-        {
-            type: todos_constants_1.TOGGLE_TODO,
-            payload: {
-                todo: {
-                    name: 'Task#2',
-                    completed: true,
-                    id: 1
-                }
-            },
-            version: '0.1',
-            createDate: new Date('2017-05-26T12:21:00'),
-            description: 'Change Task#2 status to complete',
-            applied: true
-        },
-        {
-            type: todos_constants_1.REMOVE_TODO,
-            payload: {
-                id: 0
-            },
-            version: '0.1',
-            createDate: new Date('2017-05-26T13:01:00'),
-            description: 'Remove Task#1 from todo list',
-            applied: true
+                },
+                version: '0.1',
+                createDate: new Date('2017-05-26T13:01:00'),
+                description: 'Remove Task#1 from todo list',
+                applied: true
+            }
+        ];
+        if (!localStorage.getItem('actions')) {
+            localStorage.setItem('actions', JSON.stringify(actions));
         }
-    ];
+    }
+    saveInitDataToLocalStorage();
     angular.module(TodoMockModule.name, ['ngMockE2E'])
         .run(function ($httpBackend) {
         $httpBackend.when('POST', /.*\/todos\/actions\/$/)
             .respond(function (method, url, data) {
             var action = JSON.parse(data);
-            TodoMockModule.actions.push(action);
+            var actions = JSON.parse(localStorage.getItem('actions'));
+            actions.push(action);
+            localStorage.setItem('actions', JSON.stringify(actions));
             return [200];
         });
         $httpBackend.when('GET', /.*\/todos\/actions\/$/)
-            .respond(function () { return [200, TodoMockModule.actions]; });
+            .respond(function () { return [200, localStorage.getItem('actions')]; });
         $httpBackend.when('GET', /.*/).passThrough();
         $httpBackend.when('POST', /.*/).passThrough();
         $httpBackend.when('PUT', /.*/).passThrough();
--- src/components/todos/todo.mock.module.ts
+++ src/components/todos/todo.mock.module.ts
@@ -10,60 +10,67 @@ import {ADD_TODO, REMOVE_TODO, TOGGLE_TODO} from "../../constants/todos.constant
 export module TodoMockModule {
     export const name: string = 'app.todo.mock';
 
-    export const actions = [
-        {
-            type: ADD_TODO,
-            payload: {
-                todo: {
-                    name: 'Task#1',
-                    completed: false,
+    function saveInitDataToLocalStorage() {
+        const actions = [
+            {
+                type: ADD_TODO,
+                payload: {
+                    todo: {
+                        name: 'Task#1',
+                        completed: false,
+                        id: 0
+                    }
+                },
+                version: '0.1',
+                createDate: new Date('2017-05-26T12:00:00'),
+                description: 'Add Task#1 to todo list',
+                applied: true
+            },
+            {
+                type: ADD_TODO,
+                payload: {
+                    todo: {
+                        name: 'Task#2',
+                        completed: false,
+                        id: 1
+                    }
+                },
+                version: '0.1',
+                createDate: new Date('2017-05-26T12:20:00'),
+                description: 'Add Task#2 to todo list',
+                applied: true
+            },
+            {
+                type: TOGGLE_TODO,
+                payload: {
+                    todo: {
+                        name: 'Task#2',
+                        completed: true, // todo: lel
+                        id: 1
+                    }
+                },
+                version: '0.1',
+                createDate: new Date('2017-05-26T12:21:00'),
+                description: 'Change Task#2 status to complete',
+                applied: true
+            },
+            {
+                type: REMOVE_TODO,
+                payload: {
                     id: 0
-                }
-            },
-            version: '0.1',
-            createDate: new Date('2017-05-26T12:00:00'),
-            description: 'Add Task#1 to todo list',
-            applied: true
-        },
-        {
-            type: ADD_TODO,
-            payload: {
-                todo: {
-                    name: 'Task#2',
-                    completed: false,
-                    id: 1
-                }
-            },
-            version: '0.1',
-            createDate: new Date('2017-05-26T12:20:00'),
-            description: 'Add Task#2 to todo list',
-            applied: true
-        },
-        {
-            type: TOGGLE_TODO,
-            payload: {
-                todo: {
-                    name: 'Task#2',
-                    completed: true, // todo: lel
-                    id: 1
-                }
-            },
-            version: '0.1',
-            createDate: new Date('2017-05-26T12:21:00'),
-            description: 'Change Task#2 status to complete',
-            applied: true
-        },
-        {
-            type: REMOVE_TODO,
-            payload: {
-                id: 0
-            },
-            version: '0.1',
-            createDate: new Date('2017-05-26T13:01:00'),
-            description: 'Remove Task#1 from todo list',
-            applied: true
+                },
+                version: '0.1',
+                createDate: new Date('2017-05-26T13:01:00'),
+                description: 'Remove Task#1 from todo list',
+                applied: true
+            }
+        ];
+        if (!localStorage.getItem('actions')) {
+            localStorage.setItem('actions', JSON.stringify(actions));
         }
-    ];
+    }
+
+    saveInitDataToLocalStorage();
 
     angular.module(TodoMockModule.name, ['ngMockE2E'])
         .run(($httpBackend: angular.IHttpBackendService) => {
@@ -72,12 +79,14 @@ export module TodoMockModule {
             $httpBackend.when('POST', /.*\/todos\/actions\/$/)
                 .respond((method, url, data: string) => {
                     const action = JSON.parse(data);
+                    const actions = JSON.parse(localStorage.getItem('actions'));
                     actions.push(action);
+                    localStorage.setItem('actions', JSON.stringify(actions));
                     return [200];
                 });
 
             $httpBackend.when('GET', /.*\/todos\/actions\/$/)
-                .respond(() => [200, actions]);
+                .respond(() => [200, localStorage.getItem('actions')]);
 
             $httpBackend.when('GET', /.*/).passThrough();
             $httpBackend.when('POST', /.*/).passThrough();
